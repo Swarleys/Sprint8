5b49cad61d88564142320a148434cc2b6a085044
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2022-03-16 22:16:42 +0100

    Adding LogIn and Sign Up buttons

--- vue/src/components/NavBar.vue
+++ vue/src/components/NavBar.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import RegisterForm from './RegisterForm.vue';
 import LogInForm from './LogInForm.vue';
+import { useUsers } from '../stores/users';
+const storeUsers = useUsers();
 </script>
 
 
 <template>
-    <header class="fondo-logo flex justify-center">
-        <img src="../assets/starwarslogo.png" alt="StarWars logo" class="p-20 text-center" />
+    <header class="fondo-logo flex justify-between text-white items-center flex-col md:flex-row overflow-hidden">
+        <div></div>
+        <img src="../assets/starwarslogo.png" alt="StarWars logo" class="p-20 text-center md:ml-52 lg:ml-60  ml-2" />
+        <div class="sm:pr-12 pr-0 text-2xl mb-16 md:mb-0">
+            <span class="uppercase hover:cursor-pointer" @click="storeUsers.isOpenLogin = true">Log In </span>
+            <span>//</span>
+            <span class="uppercase hover:cursor-pointer" @click="storeUsers.isOpenRegister = true"> Sign up</span>
+        </div>
     </header>
     <nav class="text-slate-200 border-collapse">
         <ul class="flex justify-center border border-slate-200">
